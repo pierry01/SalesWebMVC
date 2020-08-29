7d30f7d6f15e2bfe8579232bbf30bee4cd10e5dd
Jean Mendonça <pierrybm@gmail.com>
2020-08-28 22:58:32 -0300

    MySQL adaptation and first migration

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -8,14 +8,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SalesWebMVC {
-    public class Program {
-        public static void Main(string[] args) {
+namespace SalesWebMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
